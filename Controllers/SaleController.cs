f05f8d202d2be203e63bcea26adec8ba0e935832
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-11-10 10:32:49 -0300

    feat: adding sale to order (controller - post)

--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -1,4 +1,6 @@
 using tech_test_payment_api.Contexts;
+using tech_test_payment_api.Models;
+using tech_test_payment_api.Enums;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,47 @@ namespace tech_test_payment_api.Controllers
         public SaleController(ContextOrganizer context)
         {
             _context = context;
+        }
+
+        // Adding Order Sale
+        [HttpPost("AddingOrderSale")]
+        public IActionResult AddingSale(OrderSale orderSale)
+        {
+            int countItems = 0;
+
+            foreach (var item in orderSale.Products)
+            {
+                countItems += 1;
+            }
+
+            if (countItems < 1)
+            {
+                return BadRequest("Solicitação do pedido realizada com sucesso!");
+            }
+
+            orderSale.Status = StatusTransitions.Awaiting_Payment;
+            orderSale.CurrentDate = DateTime.Now;
+
+            _context.Sellers.Add(orderSale.Seller);
+
+            decimal Amount = 0;
+
+            foreach (var item in orderSale.Products)
+            {
+                if (item.Price <= 0 || item.Inventory <= 0)
+                {
+                    return BadRequest("Preço e estoque não podem ser iguais a zero ou menores que zero!");
+                }
+
+                Amount += item.Price * item.Inventory;
+                _context.Products.Add(item);
+            }
+
+            orderSale.Amount = Amount;
+            _context.OrderSales.Add(orderSale);
+            _context.SaveChanges();
+
+            return Ok(orderSale);
         }
     }
 }
